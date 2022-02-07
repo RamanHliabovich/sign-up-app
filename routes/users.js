6f63d7d87ac1dc5f0ade35a77aa1195761701c79
Raman Hliabovich <hliaborn@mail.uc.edu>
2022-02-06 20:16:25 -0500

    cleaned up routes

--- routes/users.js
+++ routes/users.js
@@ -36,13 +36,4 @@ router.post('/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
 module.exports = router
